b73a39983421ecd903d27f57e3648ce7d6731ef7
oriolmelendez <60789135+oriolmelendez@users.noreply.github.com>
2021-02-03 20:37:46 +0100

    Add files via upload

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,31 +1,47 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from "@angular/forms";
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule  } from "@angular/common/http";
+import { HttpClientModule } from "@angular/common/http";
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatTableModule } from '@angular/material/table';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+
 
 import { routing } from './app.routing';
 import { AppComponent } from './app.component';
 import { PrincipalComponent } from './components/principal/principal.component';
 import { PlaylistComponent } from './components/playlist/playlist.component';
 import { LoginComponent } from './components/login/login.component';
-
+import { ControlComponent } from './components/control/control.component';
+import { TaulaComponent } from './components/taula/taula.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PrincipalComponent,
     PlaylistComponent,
-    LoginComponent
+    LoginComponent,
+    ControlComponent,
+    TaulaComponent
   ],
   imports: [
     BrowserModule,
     ReactiveFormsModule,
     FormsModule,
     routing,
-    HttpClientModule
+    HttpClientModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatTableModule
   ],
   providers: [],
-  bootstrap: [AppComponent,PrincipalComponent]
+  bootstrap: [AppComponent, PrincipalComponent]
 })
 export class AppModule { }
--- app/components/login/login.component.ts
+++ app/components/login/login.component.ts
@@ -1,37 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from "@angular/common/http";
-import { HttpParams } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent{
 
-  usuari: string;
-  password: string;
+  constructor() { }
 
-  constructor(private http: HttpClient) { }
-  //constructor() { }
+  validarLogin(usuari, password):void{
 
-  validarLogin(usuari:string, password: string): void{
-    var xhr = new XMLHttpRequest(); 
- 
-    /* xhr.onreadystatechange = function(){ 
-        if (this.readyState === 4) { 
-            document.getElementById("consulta").innerHTML =  this.response; 
-        } 
-    }  */
-    
-    let dades = usuari + ',' + password;
-
-    xhr.open("POST","http://localhost:8888/login",true);
-    xhr.send(dades);
-  }
-  
-
-  ngOnInit(): void {
   }
 
 }
