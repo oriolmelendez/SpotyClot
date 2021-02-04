3ab7fbde32a150dc4ff1baf2abc9a14726d93215
oriolmelendez <60789135+oriolmelendez@users.noreply.github.com>
2021-02-04 17:33:23 +0100

    Add files via upload

--- app/components/taula/taula.component.ts
+++ app/components/taula/taula.component.ts
@@ -6,22 +6,22 @@ import { MatInputModule } from '@angular/material/input';
 
 export interface PeriodicElement {
   name: string;
-  position: number;
+  //position: number;
   weight: number;
   symbol: string;
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+  {name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+  {name: 'Helium', weight: 4.0026, symbol: 'He'},
+  {name: 'Lithium', weight: 6.941, symbol: 'Li'},
+  {name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+  {name: 'Boron', weight: 10.811, symbol: 'B'},
+  {name: 'Carbon', weight: 12.0107, symbol: 'C'},
+  {name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+  {name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+  {name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+  { name: 'Neon', weight: 20.1797, symbol: 'Ne'},
 ];
 
 @Component({
@@ -30,7 +30,7 @@ const ELEMENT_DATA: PeriodicElement[] = [
   styleUrls: ['./taula.component.css']
 })
 export class TaulaComponent {
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  displayedColumns: string[] = ['name', 'weight', 'symbol'];
   dataSource = new MatTableDataSource(ELEMENT_DATA);
 
   applyFilter(event: Event) {
