8daf35b806998672ee265f9353976a13080c2d6e
oriolmelendez <60789135+oriolmelendez@users.noreply.github.com>
2021-02-08 00:50:08 +0100

    Add files via upload

--- Server.js
+++ Server.js
@@ -1,22 +1,34 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
-app.use(bodyParser.json());
 app.use(express.static('public'));
 var path = require('path');
-var cors = require('cors')
-
-app.use(cors())
 const PORT = 8888;
+var fs = require("fs");
+var Readable = require('stream').Readable;
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "*");
+    next();
+});
+//Arrel
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
-  extended: true
-}));
+app.get("/music/:song", (req, res) => {
+    console.log(req.params.song);
 
-//Creació de la partida
-app.post("/login", (req,res) => {
-    console.log(req.params);
-    res.sendStatus(200);
+
+    const filePath = path.join(__dirname, '/songs/' + req.params.song + '.mp3');
+    const stat = fs.statSync(filePath);
+
+    res.writeHead(200, {
+        'Content-Type': 'audio/mpeg',
+        'Content-Length': stat.size
+    });
+
+    const readStream = fs.createReadStream(filePath);
+    readStream.pipe(res);
 });
 
 app.listen(PORT, () => {
